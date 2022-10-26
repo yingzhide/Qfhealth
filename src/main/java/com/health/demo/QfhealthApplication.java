05769e1fe37b60b46c2d47d010ffb76befec7e1a
yingzhide <yingzhidezhanghu@gmail.com>
2022-10-26 16:35:08 +0800

    Initial commit

---
+++ src/main/java/com/health/demo/QfhealthApplication.java
@@ -0,0 +1,13 @@
+package com.health.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QfhealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QfhealthApplication.class, args);
+    }
+
+}
